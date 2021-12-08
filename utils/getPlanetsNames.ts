6e979f74453dda91fd27dd43e62774631d7d5d91
JoelFrancisco <joelfranciscofilho@gmail.com>
2021-12-08 20:32:35 -0300

    fix: removing unused file

--- utils/getPlanetsNames.ts
+++
@@ -1,9 +0,0 @@
-import { getAllPlanetsResults } from './getAllPlanetsResults';
-
-const getPlanetsNames = async () => {
-  const results = await getAllPlanetsResults();
-  const names = results.map((product) => product.name);
-  return names;
-};
-
-export { getPlanetsNames };
